2e661c4c93e3aafd979fddb889f1fdd4136f2d06
PikaDude <mail@pikadude.me>
2020-10-14 22:39:24 +1100

    a

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 module.exports = {
-    extends: "pikadude/base",
+    env: {
+        node: true,
+    },
+    extends: ['plugin:prettier/recommended', 'eslint:recommended', 'prettier'],
+    plugins: ['prettier'],
     parserOptions: {
-        ecmaVersion: 11
+        ecmaVersion: 11,
+    },
+    rules: {
+        'linebreak-style': ['error', 'unix'],
+        'no-async-promise-executor': 0,
+        'prefer-const': [
+            'error',
+            {
+                destructuring: 'any',
+                ignoreReadBeforeAssign: false,
+            },
+        ],
+        'prettier/prettier': [
+            'error',
+            {
+                semi: true,
+                arrowParens: 'always',
+                singleQuote: true,
+                tabWidth: 4,
+            },
+        ],
     },
 };
